f5de6eb0f936a3e53688b28a73ea56c78583c226
swatig2312 <swatigupta@ncash.ai>
2022-08-25 22:25:39 +0530

    modified in submodule

--- index.js
+++ index.js
@@ -6,4 +6,5 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 /* swati */
+/* test */
 AppRegistry.registerComponent(appName, () => App);
